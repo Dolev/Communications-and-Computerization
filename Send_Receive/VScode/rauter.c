7ad1fcc3cdf8c58f3b20126114a7a89f76fe35e1
Dolev .H <33119992+Dolev@users.noreply.github.com>
2018-12-27 15:54:31 +0200

    Update rauter.c

--- Send_Receive/VScode/rauter.c
+++ Send_Receive/VScode/rauter.c
@@ -8,112 +8,64 @@
 #include <strings.h>
 #include <unistd.h>
 #include <stdio.h>
-#define NAME 54321
-#include <sys/types.h>
-#include <netinet/in.h>
-#include <inttypes.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <strings.h>
-#include <unistd.h>
-#include <stdio.h>
-#define NAME 12345
-
-
-void printsin(struct sockaddr_in *s, char *str1, char *str2) {
-
-  printf("%s\n", str1);
-  printf("%s ", str2);
-  printf("ip= %s , port= %d", inet_ntoa(s->sin_addr), s->sin_port);
-  printf("\n");
-}
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
 
 
 int main(int argc, char *argv[])
 {
-  // Define varibales
-  int socket_fd, cc, fsize;
-  struct sockaddr_in  s_in, from;
-  struct { char head; u_long  body; char tail;} out_msg, in_msg;
+  int socket_fd, cc;
+  socklen_t fsize;
+  struct sockaddr_in  s_in, From,Client,Server;
+  
+  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);  //in-socket
+ // sockaddr_in
+  memset((char *) &s_in,0, sizeof(s_in));  /* They say you must do this    */
 
-
-  // Create socket for communication
-  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
-
-  bzero((char *) &s_in, sizeof(s_in));  /* They say you must do this    */
-
-  // Define the address used by the socket
   s_in.sin_family = (short)AF_INET;
-  s_in.sin_addr.s_addr = htonl(INADDR_ANY);    /* WILDCARD */
+  s_in.sin_addr.s_addr = htonl(INADDR_ANY);    
   s_in.sin_port = htons((u_short)0x3333);
+// sockaddr_in - server
+  memset((char *) &Server,0, sizeof(Server)); /* They say you must do this    */
 
-  //printsin( &s_in, "RECV_UDP", "Local socket is:"); 
-  fflush(stdout);
+  Server.sin_family = (short) AF_INET;
+  Server.sin_addr.s_addr = htonl(INADDR_ANY);
+  Server.sin_port = htons((u_short)0x3311);
+// sockaddr_in - client
+  memset((char *) &Client,0, sizeof(Client)); /* They say you must do this    */
+  
+  Client.sin_family = (short) AF_INET;
+  Client.sin_addr.s_addr = htonl(INADDR_ANY);
+  Client.sin_port = htons((u_short)0x3322);  
 
-  // Binds the unbound socket to the address given
+//bind for our socket
   bind(socket_fd, (struct sockaddr *)&s_in, sizeof(s_in));
+    
 
-  // Loops infinitly
-  for(;;) {
-    fsize = sizeof(from);
+  char msg_get[500];
+  memset(&msg_get,0,sizeof(msg_get));
+  while(1) {
+    fsize = sizeof(From);
+    cc = recvfrom(socket_fd,&msg_get,sizeof(msg_get),0,(struct sockaddr *)&From,&fsize);  //
+    msg_get[cc] = '\0';
 
-    // Receives a message and store it into in_msg varibale
-    cc = recvfrom(socket_fd,&in_msg,sizeof(in_msg),0,(struct sockaddr *)&from,&fsize);
-    printsin( &from, "recv_udp: ", "Packet from:");
+    if(From.sin_port==Client.sin_port){  // if client send to router by port
+		     
+	  double rnd = rand() % 2;
+    double x = atof(argv[1]);
+        	if(rnd > x){  //send client message       
+	           	sendto(socket_fd, msg_get, strlen(msg_get) , 0, (struct sockaddr *)&Server,sizeof(Server));
+	             }
+	        else          //dont send..
+          {   sendto(socket_fd, "Don't_Write", strlen("Don't_Write") , 0, (struct sockaddr *)&Server,sizeof(Server)); }
+    }
+    else{   //Always sending
+	        sendto(socket_fd, msg_get , strlen(msg_get) , 0, (struct sockaddr *)&Client,sizeof(Client));       
+     }
 
-    // Prints the message
-    printf("Server got data ::%c%ld%c\n",in_msg.head,(long) ntohl(in_msg.body),in_msg.tail); 
-    fflush(stdout);
-
-    // Create the message
-    out_msg.head = '<';
-    out_msg.body = htonl(NAME);
-    out_msg.tail = '>';
-
-    // Send the message to the socket 
-    sendto(socket_fd,&out_msg,sizeof(out_msg),0,(struct sockaddr *)&from, sizeof(from));
-    printf("Server sent data ::%c%ld%c\n",out_msg.head, (long) ntohl(out_msg.body), out_msg.tail); 
-    fflush(stdout);
+    memset(&msg_get,0,sizeof(msg_get));
   }
   
-  return 0;
-}
-///////////////////////////////////
-
-
-  // Define varibales
-  int socket_fd;
-  struct sockaddr_in  dest;
-  struct hostent *hostptr;
-  struct { char head; u_long  body; char tail;} out_msg, in_msg;
-
-  // Create socket for communication
-  socket_fd = socket (AF_INET, SOCK_DGRAM, 0);
-  bzero((char *) &dest, sizeof(dest)); /* They say you must do this */
-
-  // Get the host by the host name 
-  hostptr = gethostbyname(argv[1]);
-
-  // Define the address used by the socket
-  dest.sin_family = (short) AF_INET;
-  bcopy(hostptr->h_addr, (char *)&dest.sin_addr,hostptr->h_length);
-  dest.sin_port = htons((u_short)0x3333);
-
-  // Create the message
-  out_msg.head = '<';
-  out_msg.body = htonl(NAME);
-  out_msg.tail = '>';
-
-  // Send the message to the socket 
-  sendto(socket_fd,&out_msg,sizeof(out_msg),0,(struct sockaddr *)&dest,
-                  sizeof(dest));
-  printf("Client sent data ::%c%ld%c\n",out_msg.head, (long) ntohl(out_msg.body), out_msg.tail); 
-  // Receives a message and store it into in_msg varibale
-  recvfrom(socket_fd,&in_msg,sizeof(in_msg),0,(struct sockaddr *)&dest, sizeof(dest));
-
-  // Prints the message
-  printf("Client got data ::%c%ld%c\n",in_msg.head,(long) ntohl(in_msg.body),in_msg.tail); 
-
   return 0;
 }
